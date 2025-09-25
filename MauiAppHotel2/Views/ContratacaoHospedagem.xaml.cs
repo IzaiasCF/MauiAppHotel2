42841168220e248272ec094c49038265d38e0c1a
IzaiasCF <izaiascorreafilho@gmail.com>
2025-09-25 15:41:04 -0300

    Add: calendar

--- MauiAppHotel2/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel2/Views/ContratacaoHospedagem.xaml.cs
@@ -2,9 +2,21 @@ namespace MauiAppHotel2.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+	App PropriedadesApp;
+
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
+
+		PropriedadesApp = (App)Application.Current;
+
+		pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+		dtpck_checking.MinimumDate = DateTime.Now;
+		dtpck_checking.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+		dtpck_checkout.MinimumDate = dtpck_checking.Date.AddDays(1);
+		dtpck_checkout.MaximumDate = dtpck_checking.Date.AddMonths(6);
 	}
 
     private void Button_Clicked(object sender, EventArgs e)
