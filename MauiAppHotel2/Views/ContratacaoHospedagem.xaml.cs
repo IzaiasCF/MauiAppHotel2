680bd64e6b689b7a1a0b9f800e92507c5b2c4260
IzaiasCF <izaiascorreafilho@gmail.com>
2025-09-25 12:42:26 -0300

    Add: button clicked in ContratacaoHospedagem

--- MauiAppHotel2/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel2/Views/ContratacaoHospedagem.xaml.cs
@@ -6,4 +6,16 @@ public partial class ContratacaoHospedagem : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new HospedagemContratada());
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Aviso", ex.Message, "Ok");
+		}
+    }
 }
