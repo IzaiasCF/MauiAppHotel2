371bb2d864ecc020db8ade12e8689474889966b7
IzaiasCF <izaiascorreafilho@gmail.com>
2025-09-25 08:40:57 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel2/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiAppHotel2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        // Definição da largura/altura do App
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;  /*Altura*/
+            window.Height = 700;  /*Largura*/
+
+            return window;
+        }
+    }
+}
